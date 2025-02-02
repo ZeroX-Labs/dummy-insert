9aa4e7dca9d48f6a3dbbe6408b653ee0858d7e57
jirachi <daigiachantran01@gmail.com>
2025-02-02 15:28:14 +0700

    init project

---
+++ cli/src/main/java/com/zerox/Main.java
@@ -0,0 +1,10 @@
+package com.zerox;
+
+import com.zerox.core.DummyInsert;
+
+public class Main {
+    public static void main(String[] args) {
+        DummyInsert dummyInsert = new DummyInsert();
+        System.out.println("Hello world!");
+    }
+}
