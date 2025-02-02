d9d8757a7d58f7caaf69f8f88a8e8de21af677ba
jirachi <daigiachantran01@gmail.com>
2025-02-02 15:27:21 +0700

    add .gitignore

---
+++ core/src/main/java/com/zerox/core/DummyInsert.java
@@ -0,0 +1,10 @@
+package com.zerox.core;
+
+public class DummyInsert {
+    public DummyInsert() {
+        System.out.println("DummyInsert constructor");
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
